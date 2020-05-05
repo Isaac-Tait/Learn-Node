5e6c34de942ef62373bdf546e85ed892331767c8
Isaac Tait <isaac@mountaintopcoding.com>
2020-05-05 06:58:48 -0700

    stuck then un-stuck, thank you Slack

--- dang-thats-delicious/app.js
+++ dang-thats-delicious/app.js
@@ -13,24 +13,24 @@ const routes = require('./routes/index');
 const helpers = require('./helpers');
 const errorHandlers = require('./handlers/errorHandlers');
 
-// create our Express app
+//! create our Express app
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views')); // this is the folder where we keep our pug files
-app.set('view engine', 'pug'); // we use the engine pug, mustache or EJS work great too
+//! view engine setup
+app.set('views', path.join(__dirname, 'views')); //! this is the folder where we keep our pug files
+app.set('view engine', 'pug'); //! we use the engine pug, mustache or EJS work great too
 
-// serves up static files from the public folder. Anything in public/ will just be served up as the file it is
+//! serves up static files from the public folder. Anything in public/ will just be served up as the file it is
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Takes the raw requests and turns them into usable properties on req.body
+//* Takes the raw requests and turns them into usable properties on req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Exposes a bunch of methods for validating data. Used heavily on userController.validateRegister
+//? Exposes a bunch of methods for validating data. Used heavily on userController.validateRegister
 app.use(expressValidator());
 
-// populates req.cookies with any cookies that came along with the request
+// TODO populates req.cookies with any cookies that came along with the request
 app.use(cookieParser());
 
 // Sessions allow us to store data on visitors from request to request
--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const codingGenius = { name: 'Isaac', age: 48, cool: true };
+  //* res.send('Hey! It works!');
+  res.json(codingGenius);
 });
 
 module.exports = router;
