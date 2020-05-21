05eeea5c5086c8e31d8450df0808f04c95b7cefb
Isaac Tait <isaac@mountaintopcoding.com>
2020-05-21 14:06:35 -0700

    controllers & MVC pattern

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,16 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
-// Do work here
-router.get('/', (req, res) => {
-  //! const codingGenius = { name: 'Isaac', age: 48, cool: true };
-  //! res.json(codingGenius);
-  res.render('hello', {
-    name: 'Isaac', 
-    age: 43, 
-    cool: true,
-    title: 'grvlnt'
-  });
-});
+router.get('/', storeController.homePage);
 
 module.exports = router;
