0eecb70ff0549f9191e347029db5a4de6b2f8121
Isaac Tait <isaac@mountaintopcoding.com>
2020-05-22 07:13:55 -0700

    building schema Tadashi is awake, time for a break

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,9 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-    req.name = 'Isaac';
-    res.cookie('name', 'Isaac is a bodacious software programer. You should be his friend 😎', { maxAge: 9000 })
-    next();
-};
-
 exports.homePage = (req, res) => {
     console.log(req.name);
     res.render('index');
