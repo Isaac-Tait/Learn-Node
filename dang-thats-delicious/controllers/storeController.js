28e3bd4af3362c7370aea3ed71b82e0e73632c74
Isaac Tait <isaac@mountaintopcoding.com>
2020-06-02 18:18:41 -0700

    10:33 mark

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -4,5 +4,5 @@ exports.homePage = (req, res) => {
 };
 
 exports.addStore = (req, res) => { 
-    res.render('editStore', { title: 'Add Store' })
+    res.render('editStore', { title: '🏔 Add Store' })
 };
