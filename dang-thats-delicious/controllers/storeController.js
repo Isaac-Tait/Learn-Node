ea92eff946af83cf1b28c27223438a539afe5245
Isaac Tait <isaac@mountaintopcoding.com>
2020-07-11 15:29:16 -0700

    Oh yea I am rockin this shiznit

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -19,3 +19,10 @@ exports.createStore = async (req, res) => {
     req.flash('success', `You successfully created ${store.name}! Would you care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+    // 1. Query the database for a list of ALL stores
+    const stores = await Store.find();
+    //console.log(stores);
+    res.render('stores', { title: 'Stores', stores: stores })
+};
